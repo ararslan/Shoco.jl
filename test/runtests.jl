a1ade6700b32b7f98df30a1da8b709c4babc6cfe
Alex A <ararslan@comcast.net>
2016-03-09 22:22:54 -0800

    Added a couple more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,3 +5,10 @@ s = "What's up, doc?"
 c = compress(s)
 @test 0 < sizeof(c) < sizeof(s)
 @test decompress(c) == s
+
+@test compress("") == ""
+@test decompress("") == ""
+
+s = "؉'s ⎨<g"
+d = decompress(s)
+@test sizeof(d) > sizeof(s)

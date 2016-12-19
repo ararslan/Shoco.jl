faff857a34bcf2fcaf835ef6949d0c8fd6735b6d
Alex Arslan <ararslan@comcast.net>
2016-12-18 18:25:53 -0800

    Updates for efficiency, drop 0.4 support (#3)

--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,4 @@
-import Compat: @static, is_windows, is_apple
-
-@static if is_windows()
+if is_windows()
     error("The shoco C library doesn't support Windows")
 end
 
--- src/Shoco.jl
+++ src/Shoco.jl
@@ -1,55 +1,50 @@
 __precompile__()
 
 module Shoco
-    if isfile(joinpath(dirname(@__FILE__), "..", "deps", "deps.jl"))
-        include("../deps/deps.jl")
-    else
-        error("Shoco not properly installed. Please run Pkg.build(\"Shoco\")",
-              " and restart Julia.")
-    end
 
-    export compress, decompress
-
-    if !isdefined(Base, :unsafe_string)
-        const unsafe_string = bytestring
-    end
-
-    function compress(s::AbstractString)
-        isempty(s) && return ""
-
-        # The output should be no longer than the input
-        compressed = Array(Cchar, sizeof(s))
-
-        # The function modifies `compressed` and returns the number of bytes written
-        nbytes = ccall((:shoco_compress, shoco), Int,
-                       (Ptr{Cchar}, Csize_t, Ptr{Cchar}, Csize_t),
-                       s, 0, compressed, sizeof(s))
-
-        # Failed compression is shoco's fault, not ours >_>
-        nbytes > 0 || throw(ErrorException("Compression failed for input $s"))
-
-        compressed = compressed[1:nbytes]
-        compressed[end] == Cchar(0) || push!(compressed, Cchar(0))
-
-        return unsafe_string(pointer(compressed))
-    end
-
-
-    function decompress(s::AbstractString)
-        isempty(s) && return ""
-
-        # The decompressed string will be at most twice as long as the input
-        decompressed = Array(Cchar, 2 * sizeof(s))
-
-        nbytes = ccall((:shoco_decompress, shoco), Int,
-                       (Ptr{Cchar}, Csize_t, Ptr{Cchar}, Csize_t),
-                       s, sizeof(s), decompressed, 2 * sizeof(s))
-
-        nbytes > 0 || throw(ErrorException("Decompression failed for input $s"))
-
-        decompressed = decompressed[1:nbytes]
-        decompressed[end] == Cchar(0) || push!(decompressed, Cchar(0))
-
-        return unsafe_string(pointer(decompressed))
-    end
+if isfile(joinpath(dirname(@__FILE__), "..", "deps", "deps.jl"))
+    include("../deps/deps.jl")
+else
+    error("Shoco not properly installed. Please run Pkg.build(\"Shoco\")",
+          " and restart Julia.")
 end
+
+export compress, decompress
+
+function compress{T<:AbstractString}(s::T)
+    isempty(s) && return s
+
+    # The output should be no longer than the input
+    compressed = Vector{UInt8}(sizeof(s))
+
+    # The function modifies `compressed` and returns the number of bytes written
+    nbytes = ccall((:shoco_compress, shoco), Int,
+                   (Ptr{Cchar}, Csize_t, Ptr{UInt8}, Csize_t),
+                   s, 0, compressed, sizeof(s))
+
+    # Failed compression is shoco's fault, not ours >_>
+    nbytes > 0 || error("Compression failed for input $s")
+
+    resize!(compressed, nbytes)
+
+    return T(String(compressed))
+end
+
+function decompress{T<:AbstractString}(s::T)
+    isempty(s) && return s
+
+    # The decompressed string will be at most twice as long as the input
+    decompressed = Vector{UInt8}(2 * sizeof(s))
+
+    nbytes = ccall((:shoco_decompress, shoco), Int,
+                   (Ptr{Cchar}, Csize_t, Ptr{UInt8}, Csize_t),
+                   s, sizeof(s), decompressed, 2 * sizeof(s))
+
+    nbytes > 0 || error("Decompression failed for input $s")
+
+    resize!(decompressed, nbytes)
+
+    return T(String(decompressed))
+end
+
+end # module
